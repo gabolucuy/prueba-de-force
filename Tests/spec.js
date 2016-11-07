b08f909ab09d921fafe920f1a90b5f8a834d47f6
Leonardo <leonardo.velarde.ldvc@gmail.com>
2016-11-07 09:15:03 -0400

    test now opens side menu

--- Tests/spec.js
+++ Tests/spec.js
@@ -10,14 +10,9 @@ describe('Protractor Tests for cps app', function() {
 
   it('Click on side menu', function() {
     browser.get('http://localhost:8100/#/app/home');
-    element(by.css('button button-icon button-clear icon-navicon'));
-
+    var sideMenuButton = element.all(by.css('button[name="javier-menu"]')).get(1);
+    sideMenuButton.click();
+    browser.sleep(1000);
   });
-
-
-
-
-
-
 
 });
